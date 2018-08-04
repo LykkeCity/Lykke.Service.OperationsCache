3ef4b50fa0bdb3225ce3c9b3e72b7819241ce151
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-08-04 06:44:59 +0300

    LWDEV-8684 Deps updated + migration to core 2.1 finalized + new logging added to client.

--- client/Lykke.Service.OperationsCache.Client/AutofacExtension.cs
+++ client/Lykke.Service.OperationsCache.Client/AutofacExtension.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using Autofac;
 using Common.Log;
+using Lykke.Common.Log;
 
 namespace Lykke.Service.OperationsCache.Client
 {
     public static class AutofacExtension
     {
+        [Obsolete("Please, use the overload which consumes ILogFactory instead.")]
         public static void RegisterOperationsCacheClient(this ContainerBuilder builder, string serviceUrl, ILog log)
         {
             if (builder == null) throw new ArgumentNullException(nameof(builder));
@@ -20,9 +22,31 @@ namespace Lykke.Service.OperationsCache.Client
                 .SingleInstance();
         }
 
+        public static void RegisterOperationsCacheClient(this ContainerBuilder builder, string serviceUrl, ILogFactory logFactory)
+        {
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+            if (serviceUrl == null) throw new ArgumentNullException(nameof(serviceUrl));
+            if (logFactory == null) throw new ArgumentNullException(nameof(logFactory));
+            if (string.IsNullOrWhiteSpace(serviceUrl))
+                throw new ArgumentException("Value cannot be null or whitespace.", nameof(serviceUrl));
+
+            builder.RegisterType<OperationsCacheClient>()
+                .WithParameter("serviceUrl", serviceUrl)
+                .WithParameter("log", logFactory.CreateLog(nameof(OperationsCacheClient)))
+                .As<IOperationsCacheClient>()
+                .SingleInstance();
+        }
+
+        [Obsolete("Please, use the overload which consumes ILogFactory instead.")]
         public static void RegisterOperationsCacheClient(this ContainerBuilder builder, OperationsCacheServiceClientSettings settings, ILog log)
         {
             builder.RegisterOperationsCacheClient(settings?.ServiceUrl, log);
+        }
+
+        public static void RegisterOperationsCacheClient(this ContainerBuilder builder, OperationsCacheServiceClientSettings settings, ILogFactory logFactory)
+        {
+            if (settings == null) throw new ArgumentNullException(nameof(settings));
+            builder.RegisterOperationsCacheClient(settings.ServiceUrl, logFactory);
         }
     }
 }
